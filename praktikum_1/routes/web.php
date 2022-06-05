e0d0d7a168a3023434cf2c8e8cf9e93c44ae43f8
MrMark17 <rizqinatamardika@students.amikom.ac.id>
2022-06-05 10:06:16 +0700

    Menambahkan route

--- praktikum_1/routes/web.php
+++ praktikum_1/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MahasiswaController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/cek-object', [MahasiswaController::class,'cekObject']);
+Route::get('/insert', [MahasiswaController::class,'insert']);
